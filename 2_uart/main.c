e6edf0beff7a1fb242add37205186088d20f28b0
hebe2602 <henrikstoklandberg@gmail.com>
2021-04-19 18:16:26 +0200

    ferdig oppg 3

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -3,67 +3,74 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
-
-ssize_t _write(int fd, const void *buf, size_t count){
-char * letter = (char *)(buf);
-for(int i = 0; i < count; i++){
-uart_send(*letter);
-letter++;
+ssize_t _write(int fd, const void *buf, size_t count)
+{
+	char *letter = (char *)(buf);
+	for (int i = 0; i < count; i++)
+	{
+		uart_send(*letter);
+		letter++;
+	}
+	return count;
 }
-return count;
-}
 
-int main(){
-    uart_init();
+int main()
+{
+	uart_init();
 
-    // Configure LED Matrix
-	for(int i = 4; i <= 15; i++){
+	// Configure LED Matrix
+	for (int i = 4; i <= 15; i++)
+	{
 		GPIO->DIRSET = (1 << i);
 		GPIO->OUTCLR = (1 << i);
 	}
 
-    // Configure buttons
-    GPIO->PIN_CNF[17] = 0;
-    GPIO->PIN_CNF[26] = 0;		
+	// Configure buttons
+	GPIO->PIN_CNF[17] = 0;
+	GPIO->PIN_CNF[26] = 0;
 
 	int sleep = 0;
-    int status = 0;
-	while(1){
-        
-		if(!(GPIO->IN & (1 << 26))) {
-            uart_send('b');
+	int status = 0;
+	while (1)
+	{
+
+		if (!(GPIO->IN & (1 << 26)))
+		{
+			uart_send('b');
 		}
-		if(!(GPIO->IN & (1 << 17))) {
-            uart_send('a');
+		if (!(GPIO->IN & (1 << 17)))
+		{
+			uart_send('a');
 		}
-        
-        char c = uart_read();
-        // Check if button B is pressed;
-		// turn on LED matrix if it is. 
-		if(!status && c != '\0') {
-			GPIO->OUTSET = (1<<13);
-			GPIO->OUTSET = (1<<14);
-			GPIO->OUTSET = (1<<15);
-            status = 1;
+
+		char c = uart_read();
+		// Check if button B is pressed;
+		// turn on LED matrix if it is.
+		if (!status && c != '\0')
+		{
+			GPIO->OUTSET = (1 << 13);
+			GPIO->OUTSET = (1 << 14);
+			GPIO->OUTSET = (1 << 15);
+			status = 1;
 		}
 
 		// Check if button A is pressed;
-		// turn off LED matrix if it is. 
-		else if(status && c != '\0') {
-			GPIO->OUTCLR = (1<<13);
-			GPIO->OUTCLR = (1<<14);
-			GPIO->OUTCLR = (1<<15);
-            status = 0;
+		// turn off LED matrix if it is.
+		else if (status && c != '\0')
+		{
+			GPIO->OUTCLR = (1 << 13);
+			GPIO->OUTCLR = (1 << 14);
+			GPIO->OUTCLR = (1 << 15);
+			status = 0;
 		}
 
-        sleep = 1000;
-        while(sleep) {
-            sleep--;
-        }
+		sleep = 1000;
+		while (sleep)
+		{
+			sleep--;
+		}
 
-        iprintf("The average grade in TTK%d was in %d and %d: %c\n\r",4235,2019,2018,'C');
-
+		//iprintf("The average grade in TTK%d was in %d and %d: %c\n\r",4235,2019,2018,'C');
 	}
 
 	return 0;
--- 3_gpiote/main.c
+++ 3_gpiote/main.c
@@ -2,3 +2,31 @@
 #include "ppi.h"
 #include "gpiote.h"
 
+
+int main() {
+
+    for(int i = 4; i <= 15; i++){
+            GPIO->DIRSET = (1 << i);
+            GPIO->OUTCLR = (1 << i);
+    }
+
+    GPIOTE->CONFIG[0] = 1 | (17 << 8) | (2 << 16);
+    GPIOTE->CONFIG[1] = 3 | (13 << 8) | (3 << 16);
+    GPIOTE->CONFIG[2] = 3 | (14 << 8) | (3 << 16);
+    GPIOTE->CONFIG[3] = 3 | (15 << 8) | (3 << 16);
+
+    PPI->PPI_CH[0].EEP = (uint32_t)&(GPIOTE->IN[0]);
+    PPI->PPI_CH[0].TEP = (uint32_t)&(GPIOTE->OUT[1]);
+    PPI->PPI_CH[1].EEP = (uint32_t)&(GPIOTE->IN[0]);
+    PPI->PPI_CH[1].TEP = (uint32_t)&(GPIOTE->OUT[2]);
+    PPI->PPI_CH[2].EEP = (uint32_t)&(GPIOTE->IN[0]);
+    PPI->PPI_CH[2].TEP = (uint32_t)&(GPIOTE->OUT[3]);
+
+    PPI->CHENSET = 0b111;
+
+    while(1) {
+
+    }
+
+return 0;
+}
